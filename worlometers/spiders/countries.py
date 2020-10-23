bfaab8535fc28329754c4436cee1e5d780670630
hask777 <haskellisp@gmail.com>
2020-10-23 11:20:22 -0400

    absolute & relative url

--- worlometers/spiders/countries.py
+++ worlometers/spiders/countries.py
@@ -3,7 +3,7 @@ import scrapy
 
 class CountriesSpider(scrapy.Spider):
     name = 'countries'
-    allowed_domains = ['www.worldometers.info/']
+    allowed_domains = ['www.worldometers.info']
     start_urls = ['https://www.worldometers.info/world-population/population-by-country/']
 
     def parse(self, response):
@@ -13,7 +13,8 @@ class CountriesSpider(scrapy.Spider):
             name = country.xpath(".//text()").get()
             link = country.xpath(".//@href").get()
 
-            yield {
-                'country_name': name,
-                'country_link': link
-            }
+            # absolute_url = f"https://www.worldometers.info{link}"
+            # absolute_url = response.urljoin(link)
+
+            # yield scrapy.Request(url=absolute_url)
+            yield response.follow(url=link)
